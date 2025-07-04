fcfca35c57e55c6387a104acdb3cbf0838291bd7
ran71425868 <2240182@ecc.ac.jp>
2025-07-04 15:27:48 +0900

    selectボタン追加

--- Source/SceneSelect.cpp
+++ Source/SceneSelect.cpp
@@ -31,10 +31,21 @@ void SceneSelect::Update(float elapsedTime)
 	const MouseButton anyButton =
 		Mouse::BTN_LEFT;
 
-	DirectX::XMFLOAT2 left = { 640,360 };
-	DirectX::XMFLOAT2 right = { 740,460 };
+	DirectX::XMFLOAT2 stageleft = { 320,360 };
+	DirectX::XMFLOAT2 stageright = { 420,460 };
 
-	if (left.x < mouseCursor.GetPositionX() && right.x > mouseCursor.GetPositionX()&&left.y<mouseCursor.GetPositionY()&&right.y>mouseCursor.GetPositionY())
+	DirectX::XMFLOAT2 tutorialleft = { 960,360 };
+	DirectX::XMFLOAT2 tutorialright = { 1060,460 };
+
+	if (stageleft.x < mouseCursor.GetPositionX() && stageright.x > mouseCursor.GetPositionX()&&stageleft.y<mouseCursor.GetPositionY()&&stageright.y>mouseCursor.GetPositionY())
+	{
+		if (mouseCursor.GetButtonDown() & anyButton)
+		{
+			SceneManager::Instance().ChangeScene(new SceneLoading(new SceneGame));
+		}
+	}
+
+	if (tutorialleft.x < mouseCursor.GetPositionX() && tutorialright.x > mouseCursor.GetPositionX() && tutorialleft.y<mouseCursor.GetPositionY() && tutorialright.y>mouseCursor.GetPositionY())
 	{
 		if (mouseCursor.GetButtonDown() & anyButton)
 		{
@@ -62,7 +73,12 @@ void SceneSelect::Render()
 		float screenWidth = static_cast<float>(graphics.GetScreenWidth());
 		float screenHeight = static_cast<float>(graphics.GetScreenHeight());
 		sprite->Render(rc,
-			640, 360, 0, 100, 100,
+			320, 360, 0, 100, 100,
+			0,
+			1, 1, 1, 1);
+
+		sprite->Render(rc,
+			960, 360, 0, 100, 100,
 			0,
 			1, 1, 1, 1);
 	}
